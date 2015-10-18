4e6d8f5513243ded89e76c66230ac05b1e924885
Honza Horak <hhorak@redhat.com>
2015-10-18 19:42:44 +0200

    Apply several changes from mysql 5.6

--- centos7.rh-mariadb100/contrib/common.sh
+++ centos7.rh-mariadb100/contrib/common.sh
@@ -20,13 +20,14 @@ mysql_password_regex='^[a-zA-Z0-9_~!@#$%^&*()-=<>,.?;:|]+$'
 
 function usage() {
   [ $# == 2 ] && echo "error: $1"
-  echo "You must specify following environment variables:"
+  echo "You must either specify the following environment variables:"
   echo "  MYSQL_USER (regex: '$mysql_identifier_regex')"
   echo "  MYSQL_PASSWORD (regex: '$mysql_password_regex')"
   echo "  MYSQL_DATABASE (regex: '$mysql_identifier_regex')"
-  echo "Optional:"
+  echo "Or the following environment variable:"
   echo "  MYSQL_ROOT_PASSWORD (regex: '$mysql_password_regex')"
-  echo "Settings:"
+  echo "Or both."
+  echo "Optional Settings:"
   echo "  MYSQL_LOWER_CASE_TABLE_NAMES (default: 0)"
   echo "  MYSQL_MAX_CONNECTIONS (default: 151)"
   echo "  MYSQL_FT_MIN_WORD_LEN (default: 4)"
@@ -36,17 +37,30 @@ function usage() {
 }
 
 function validate_variables() {
-  if ! [[ -v MYSQL_USER && -v MYSQL_PASSWORD && -v MYSQL_DATABASE ]]; then
+  # Check basic sanity of specified variables
+  if [[ -v MYSQL_USER && -v MYSQL_PASSWORD && -v MYSQL_DATABASE ]]; then
+    [[ "$MYSQL_USER"     =~ $mysql_identifier_regex ]] || usage "Invalid MySQL username"
+    [ ${#MYSQL_USER} -le 16 ] || usage "MySQL username too long (maximum 16 characters)"
+    [[ "$MYSQL_PASSWORD" =~ $mysql_password_regex   ]] || usage "Invalid password"
+    [[ "$MYSQL_DATABASE" =~ $mysql_identifier_regex ]] || usage "Invalid database name"
+    [ ${#MYSQL_DATABASE} -le 64 ] || usage "Database name too long (maximum 64 characters)"
+    user_specified=1
+  fi
+
+  if [ -v MYSQL_ROOT_PASSWORD ]; then
+    [[ "$MYSQL_ROOT_PASSWORD" =~ $mysql_password_regex ]] || usage "Invalid root password"
+    root_specified=1
+  fi
+
+  # Either combination of user/pass/db or root password is ok
+  if [[ "${user_specified:-0}" == "0" && "${root_specified:-0}" == "0" ]]; then
     usage
   fi
 
-  [[ "$MYSQL_USER"     =~ $mysql_identifier_regex ]] || usage "Invalid MySQL username"
-  [ ${#MYSQL_USER} -le 16 ] || usage "MySQL username too long (maximum 16 characters)"
-  [[ "$MYSQL_PASSWORD" =~ $mysql_password_regex   ]] || usage "Invalid password"
-  [[ "$MYSQL_DATABASE" =~ $mysql_identifier_regex ]] || usage "Invalid database name"
-  [ ${#MYSQL_DATABASE} -le 64 ] || usage "Database name too long (maximum 64 characters)"
-  if [ -v MYSQL_ROOT_PASSWORD ]; then
-    [[ "$MYSQL_ROOT_PASSWORD" =~ $mysql_password_regex ]] || usage "Invalid root password"
+  # Specifically check of incomplete specification
+  if [[ -v MYSQL_USER || -v MYSQL_PASSWORD || -v MYSQL_DATABASE ]] && \
+     [[ "${user_specified:-0}" == "0" ]]; then
+    usage
   fi
 }
 
@@ -87,15 +101,25 @@ function initialize_database() {
   mysql_install_db --rpm --datadir=$MYSQL_DATADIR
   start_local_mysql "$@"
 
-  [ -v MYSQL_DISABLE_CREATE_DB ] && return
+  [ -v MYSQL_RUNNING_AS_SLAVE ] && return
 
-  mysqladmin $admin_flags create "${MYSQL_DATABASE}"
-
+  # Do not care what option is compulsory here, just create what is specified
+  if [ -v MYSQL_USER ]; then
 mysql $mysql_flags <<EOSQL
     CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;
-    FLUSH PRIVILEGES ;
 EOSQL
+  fi
+
+  if [ -v MYSQL_DATABASE ]; then
+    mysqladmin $admin_flags create "${MYSQL_DATABASE}"
+
+    if [ -v MYSQL_USER ]; then
+mysql $mysql_flags <<EOSQL
+      GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;
+      FLUSH PRIVILEGES ;
+EOSQL
+    fi
+  fi
 
   if [ -v MYSQL_ROOT_PASSWORD ]; then
 mysql $mysql_flags <<EOSQL
@@ -115,42 +139,26 @@ function server_id() {
 }
 
 function wait_for_mysql_master() {
-  local master_addr=""
-  while [ true ]; do
-    master_addr=$(mysql_master_addr)
-    [ ! -z "${master_addr}" ] && break
-    echo "Waiting for MySQL master service ..."
-    sleep 1
-  done
-  echo "Got MySQL master service address: ${master_addr}"
-  while [ true ]; do
-    mysqladmin --host=${master_addr} --user="${MYSQL_MASTER_USER}" \
+  while true; do
+    echo "Waiting for MySQL master (${MYSQL_MASTER_SERVICE_NAME}) to accept connections ..."
+    mysqladmin --host=${MYSQL_MASTER_SERVICE_NAME} --user="${MYSQL_MASTER_USER}" \
       --password="${MYSQL_MASTER_PASSWORD}" ping &>/dev/null && return 0
-    echo "Waiting for MySQL master (${master_addr}) to accept connections ..."
     sleep 1
   done
-}
-
-# mysql_master_addr lookups the 'mysql-master' DNS and get list of the available
-# endpoints. Each endpoint is a MySQL container with the 'master' MySQL running.
-function mysql_master_addr() {
-  local service_name=${MYSQL_MASTER_SERVICE_NAME:-mysql-master}
-  local endpoints=$(dig ${service_name} A +search +short 2>/dev/null)
-  # FIXME: This is for debugging (docker run)
-  if [ -v MYSQL_MASTER_IP ]; then
-    endpoints=${MYSQL_MASTER_IP-}
-  fi
-  echo -n "$(echo $endpoints | cut -d ' ' -f 1)"
 }
 
 function validate_replication_variables() {
-  if ! [[ -v MYSQL_MASTER_USER && -v MYSQL_MASTER_PASSWORD  ]]; then
+  if ! [[ -v MYSQL_DATABASE && -v MYSQL_MASTER_USER && -v MYSQL_MASTER_PASSWORD && \
+        ( "${MYSQL_RUNNING_AS_SLAVE:-0}" != "1" || -v MYSQL_MASTER_SERVICE_NAME ) ]]; then
     echo
     echo "For master/slave replication, you have to specify following environment variables:"
+    echo "  MYSQL_MASTER_SERVICE_NAME (slave only)"
+    echo "  MYSQL_DATABASE"
     echo "  MYSQL_MASTER_USER"
     echo "  MYSQL_MASTER_PASSWORD"
     echo
   fi
+  [[ "$MYSQL_DATABASE" =~ $mysql_identifier_regex ]] || usage "Invalid database name"
   [[ "$MYSQL_MASTER_USER"     =~ $mysql_identifier_regex ]] || usage "Invalid MySQL master username"
   [ ${#MYSQL_MASTER_USER} -le 16 ] || usage "MySQL master username too long (maximum 16 characters)"
   [[ "$MYSQL_MASTER_PASSWORD" =~ $mysql_password_regex   ]] || usage "Invalid MySQL master password"
--- centos7.rh-mariadb100/run-mysqld.sh
+++ centos7.rh-mariadb100/run-mysqld.sh
@@ -2,20 +2,21 @@
 
 source $HOME/common.sh
 set -eu
-cmd="$1"; shift
 
 mysql_flags="-u root --socket=/tmp/mysql.sock"
 admin_flags="--defaults-file=$MYSQL_DEFAULTS_FILE $mysql_flags"
 
-if [ "$cmd" == "mysqld-master" ] &&  [ ! -d "${MYSQL_DATADIR}/mysql" ]; then
+cmd="$1"; shift
+
+if [ "${cmd}" == "mysqld-master" ] &&  [ ! -d "${MYSQL_DATADIR}/mysql" ]; then
   exec /usr/local/bin/run-mysqld-master.sh "$@"
 fi
 
-if [ "$cmd" == "mysqld-slave" ] &&  [ ! -d "${MYSQL_DATADIR}/mysql" ]; then
+if [ "${cmd}" == "mysqld-slave" ] &&  [ ! -d "${MYSQL_DATADIR}/mysql" ]; then
   exec /usr/local/bin/run-mysqld-slave.sh "$@"
 fi
 
-if [ "$cmd" == "mysqld" ]; then
+if [ "${cmd}" == "mysqld" ]; then
   validate_variables
   envsubst < ${MYSQL_DEFAULTS_FILE}.template > $MYSQL_DEFAULTS_FILE
 
@@ -27,9 +28,11 @@ if [ "$cmd" == "mysqld" ]; then
 
 # Set the password for MySQL user and root everytime this container is started.
 # This allows to change the password by editing the deployment configuration.
-mysql $mysql_flags <<EOSQL
-  SET PASSWORD FOR '${MYSQL_USER}'@'%' = PASSWORD('${MYSQL_PASSWORD}');
+if [[ -v MYSQL_USER && -v MYSQL_PASSWORD ]]; then
+  mysql $mysql_flags <<EOSQL
+    SET PASSWORD FOR '${MYSQL_USER}'@'%' = PASSWORD('${MYSQL_PASSWORD}');
 EOSQL
+fi
 
 # The MYSQL_ROOT_PASSWORD is optional, therefore we need to either enable remote
 # access with a password if the variable is set or disable remote access otherwise.
