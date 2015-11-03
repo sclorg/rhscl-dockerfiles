ff4db7a8277ac5ba0ace8e67e77164843b042dc7
Honza Horak <hhorak@redhat.com>
2015-11-03 07:24:29 +0100

    Apply resent changes in mysql image to mariadb image

--- rhel7.rh-mariadb100/copy-from-mysql.sh
+++ rhel7.rh-mariadb100/copy-from-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git rm -r contrib
-cp  ../centos7.rh-mysql56/run-mysqld* .
-cp -r ../centos7.rh-mysql56/contrib contrib
-grep -r mysql56 . | cut -d':' -f1 | while read f ; do sed -i -e 's/rh-mysql56/rh-mariadb100/g' $f ; done
-
+git rm -r root test
+cp  -r ../centos7.rh-mysql56/test .
+cp  -r ../centos7.rh-mysql56/root .
+grep -r mysql56 . | cut -d':' -f1 | while read f ; do sed -i -e 's/rh-mariadb100/rh-mariadb100/g' $f ; done
+git add root test
