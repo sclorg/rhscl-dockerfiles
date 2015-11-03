86adbab2e2ed9378b2d86eb0e47ab247141eb9a3
Honza Horak <hhorak@redhat.com>
2015-11-03 07:16:02 +0100

    Refreshed content for the following images: * centos6.devtoolset-3 * centos6.devtoolset-3-dyninst * centos6.devtoolset-3-eclipse * centos6.devtoolset-3-elfutils * centos6.devtoolset-3-oprofile * centos6.devtoolset-3-systemtap * centos6.devtoolset-3-toolchain * centos6.devtoolset-3-valgrind * centos6.devtoolset-4 * centos6.devtoolset-4-dyninst * centos6.devtoolset-4-elfutils * centos6.devtoolset-4-oprofile * centos6.devtoolset-4-systemtap * centos6.devtoolset-4-toolchain * centos6.devtoolset-4-valgrind * centos7.devtoolset-3 * centos7.devtoolset-3-dyninst * centos7.devtoolset-3-eclipse * centos7.devtoolset-3-elfutils * centos7.devtoolset-3-oprofile * centos7.devtoolset-3-systemtap * centos7.devtoolset-3-toolchain * centos7.devtoolset-3-valgrind * centos7.devtoolset-4 * centos7.devtoolset-4-dyninst * centos7.devtoolset-4-elfutils * centos7.devtoolset-4-oprofile * centos7.devtoolset-4-systemtap * centos7.devtoolset-4-toolchain * centos7.devtoolset-4-valgrind * rhel6.devtoolset-3 * rhel6.devtoolset-3-dyninst * rhel6.devtoolset-3-eclipse * rhel6.devtoolset-3-elfutils * rhel6.devtoolset-3-oprofile * rhel6.devtoolset-3-systemtap * rhel6.devtoolset-3-toolchain * rhel6.devtoolset-3-valgrind * rhel6.devtoolset-4 * rhel6.devtoolset-4-dyninst * rhel6.devtoolset-4-elfutils * rhel6.devtoolset-4-oprofile * rhel6.devtoolset-4-systemtap * rhel6.devtoolset-4-toolchain * rhel6.devtoolset-4-valgrind * rhel7.devtoolset-3 * rhel7.devtoolset-3-dyninst * rhel7.devtoolset-3-eclipse * rhel7.devtoolset-3-elfutils * rhel7.devtoolset-3-oprofile * rhel7.devtoolset-3-systemtap * rhel7.devtoolset-3-toolchain * rhel7.devtoolset-3-valgrind * rhel7.devtoolset-4 * rhel7.devtoolset-4-dyninst * rhel7.devtoolset-4-elfutils * rhel7.devtoolset-4-oprofile * rhel7.devtoolset-4-systemtap * rhel7.devtoolset-4-toolchain * rhel7.devtoolset-4-valgrind * centos7.mysql55 * rhel7.mysql55 * centos7.rh-mysql56 * centos7.postgresql92 * rhel7.postgresql92 * centos7.rh-postgresql94 * centos7.mongodb24 * rhel7.mongodb24 * centos7.rh-mongodb26 * centos7.rh-python34 * centos7.python33 * rhel7.python33 * centos7.python27 * centos7.perl516 * rhel7.perl516 * centos7.rh-perl520 * centos7.ruby20 * rhel7.ruby20 * centos7.rh-ruby22 * centos7.nodejs010 * centos7.php55 * rhel7.php55 * centos7.rh-php56

---
+++ centos7.mysql55/root/usr/share/container-scripts/mysql/common.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+# Data directory where MySQL database files live. The data subdirectory is here
+# because .bashrc and my.cnf both live in /var/lib/mysql/ and we don't want a
+# volume to override it.
+export MYSQL_DATADIR=/var/lib/mysql/data
+
+# Configuration settings.
+export MYSQL_DEFAULTS_FILE=/etc/my.cnf
+export MYSQL_LOWER_CASE_TABLE_NAMES=${MYSQL_LOWER_CASE_TABLE_NAMES:-0}
+export MYSQL_MAX_CONNECTIONS=${MYSQL_MAX_CONNECTIONS:-151}
+export MYSQL_FT_MIN_WORD_LEN=${MYSQL_FT_MIN_WORD_LEN:-4}
+export MYSQL_FT_MAX_WORD_LEN=${MYSQL_FT_MAX_WORD_LEN:-20}
+export MYSQL_AIO=${MYSQL_AIO:-1}
+
+# Be paranoid and stricter than we should be.
+# https://dev.mysql.com/doc/refman/en/identifiers.html
+mysql_identifier_regex='^[a-zA-Z0-9_]+$'
+mysql_password_regex='^[a-zA-Z0-9_~!@#$%^&*()-=<>,.?;:|]+$'
+
+# Variables that are used to connect to local mysql during initialization
+mysql_flags="-u root --socket=/tmp/mysql.sock"
+admin_flags="--defaults-file=$MYSQL_DEFAULTS_FILE $mysql_flags"
+
+# Make sure env variables don't propagate to mysqld process.
+function unset_env_vars() {
+  unset MYSQL_USER MYSQL_PASSWORD MYSQL_DATABASE MYSQL_ROOT_PASSWORD
+}
+
+# Poll until MySQL responds to our ping.
+function wait_for_mysql() {
+  pid=$1 ; shift
+
+  while [ true ]; do
+    if [ -d "/proc/$pid" ]; then
+      mysqladmin --socket=/tmp/mysql.sock ping &>/dev/null && return 0
+    else
+      return 1
+    fi
+    echo "Waiting for MySQL to start ..."
+    sleep 1
+  done
+}
+
+function start_local_mysql() {
+  # Now start mysqld and add appropriate users.
+  echo 'Starting local mysqld server ...'
+  ${MYSQL_PREFIX}/libexec/mysqld \
+    --defaults-file=$MYSQL_DEFAULTS_FILE \
+    --skip-networking --socket=/tmp/mysql.sock "$@" &
+  mysql_pid=$!
+  wait_for_mysql $mysql_pid
+}
+
+# Initialize the MySQL database (create user accounts and the initial database)
+function initialize_database() {
+  echo 'Running mysql_install_db ...'
+  # Using --rpm since we need mysql_install_db behaves as in RPM
+  mysql_install_db --rpm --datadir=$MYSQL_DATADIR
+  start_local_mysql "$@"
+
+  mysqladmin $admin_flags -f drop test
+
+  [ -v MYSQL_RUNNING_AS_SLAVE ] && return
+
+  if [ -v MYSQL_RUNNING_AS_MASTER ]; then
+    # Save master status into a separate database.
+    STATUS_INFO=$(mysql $admin_flags -e 'SHOW MASTER STATUS\G')
+    BINLOG_POSITION=$(echo "$STATUS_INFO" | grep 'Position:' | head -n 1 | sed -e 's/^\s*Position: //')
+    BINLOG_FILE=$(echo "$STATUS_INFO" | grep 'File:' | head -n 1 | sed -e 's/^\s*File: //')
+
+    mysqladmin $admin_flags create replication
+mysql $admin_flags <<EOSQL
+    use replication
+    CREATE TABLE replication (File VARCHAR(1024), Position VARCHAR(256));
+    INSERT INTO replication (File, Position) VALUES ('$BINLOG_FILE', '$BINLOG_POSITION');
+EOSQL
+  fi
+
+  # Do not care what option is compulsory here, just create what is specified
+  if [ -v MYSQL_USER ]; then
+mysql $mysql_flags <<EOSQL
+    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+EOSQL
+  fi
+
+  if [ -v MYSQL_DATABASE ]; then
+    mysqladmin $admin_flags create "${MYSQL_DATABASE}"
+
+    if [ -v MYSQL_USER ]; then
+mysql $mysql_flags <<EOSQL
+      GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;
+      FLUSH PRIVILEGES ;
+EOSQL
+    fi
+  fi
+
+  if [ -v MYSQL_ROOT_PASSWORD ]; then
+mysql $mysql_flags <<EOSQL
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+EOSQL
+  fi
+}
+
+# The 'server_id' number for slave needs to be within 1-4294967295 range.
+# This function will take the 'hostname' if the container, hash it and turn it
+# into the number.
+# See: https://dev.mysql.com/doc/refman/en/replication-options.html#option_mysqld_server-id
+function server_id() {
+  checksum=$(sha256sum <<< $(hostname -i))
+  checksum=${checksum:0:14}
+  echo -n $((0x${checksum}%4294967295))
+}
+
+function wait_for_mysql_master() {
+  while true; do
+    echo "Waiting for MySQL master (${MYSQL_MASTER_SERVICE_NAME}) to accept connections ..."
+    mysqladmin --host=${MYSQL_MASTER_SERVICE_NAME} --user="${MYSQL_MASTER_USER}" \
+      --password="${MYSQL_MASTER_PASSWORD}" ping &>/dev/null && return 0
+    sleep 1
+  done
+}
+
+
---
+++ centos7.rh-mysql56/root/usr/share/container-scripts/mysql/common.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Data directory where MySQL database files live. The data subdirectory is here
+# because .bashrc and my.cnf both live in /var/lib/mysql/ and we don't want a
+# volume to override it.
+export MYSQL_DATADIR=/var/lib/mysql/data
+
+# Configuration settings.
+export MYSQL_DEFAULTS_FILE=/etc/my.cnf
+export MYSQL_LOWER_CASE_TABLE_NAMES=${MYSQL_LOWER_CASE_TABLE_NAMES:-0}
+export MYSQL_MAX_CONNECTIONS=${MYSQL_MAX_CONNECTIONS:-151}
+export MYSQL_FT_MIN_WORD_LEN=${MYSQL_FT_MIN_WORD_LEN:-4}
+export MYSQL_FT_MAX_WORD_LEN=${MYSQL_FT_MAX_WORD_LEN:-20}
+export MYSQL_AIO=${MYSQL_AIO:-1}
+
+# Be paranoid and stricter than we should be.
+# https://dev.mysql.com/doc/refman/en/identifiers.html
+mysql_identifier_regex='^[a-zA-Z0-9_]+$'
+mysql_password_regex='^[a-zA-Z0-9_~!@#$%^&*()-=<>,.?;:|]+$'
+
+# Variables that are used to connect to local mysql during initialization
+mysql_flags="-u root --socket=/tmp/mysql.sock"
+admin_flags="--defaults-file=$MYSQL_DEFAULTS_FILE $mysql_flags"
+
+# Make sure env variables don't propagate to mysqld process.
+function unset_env_vars() {
+  unset MYSQL_USER MYSQL_PASSWORD MYSQL_DATABASE MYSQL_ROOT_PASSWORD
+}
+
+# Poll until MySQL responds to our ping.
+function wait_for_mysql() {
+  pid=$1 ; shift
+
+  while [ true ]; do
+    if [ -d "/proc/$pid" ]; then
+      mysqladmin --socket=/tmp/mysql.sock ping &>/dev/null && return 0
+    else
+      return 1
+    fi
+    echo "Waiting for MySQL to start ..."
+    sleep 1
+  done
+}
+
+function start_local_mysql() {
+  # Now start mysqld and add appropriate users.
+  echo 'Starting local mysqld server ...'
+  ${MYSQL_PREFIX}/libexec/mysqld \
+    --defaults-file=$MYSQL_DEFAULTS_FILE \
+    --skip-networking --socket=/tmp/mysql.sock "$@" &
+  mysql_pid=$!
+  wait_for_mysql $mysql_pid
+}
+
+# Initialize the MySQL database (create user accounts and the initial database)
+function initialize_database() {
+  echo 'Running mysql_install_db ...'
+  # Using --rpm since we need mysql_install_db behaves as in RPM
+  mysql_install_db --rpm --datadir=$MYSQL_DATADIR
+  start_local_mysql "$@"
+
+  [ -v MYSQL_RUNNING_AS_SLAVE ] && return
+
+  # Do not care what option is compulsory here, just create what is specified
+  if [ -v MYSQL_USER ]; then
+mysql $mysql_flags <<EOSQL
+    CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+EOSQL
+  fi
+
+  if [ -v MYSQL_DATABASE ]; then
+    mysqladmin $admin_flags create "${MYSQL_DATABASE}"
+
+    if [ -v MYSQL_USER ]; then
+mysql $mysql_flags <<EOSQL
+      GRANT ALL ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' ;
+      FLUSH PRIVILEGES ;
+EOSQL
+    fi
+  fi
+
+  if [ -v MYSQL_ROOT_PASSWORD ]; then
+mysql $mysql_flags <<EOSQL
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+EOSQL
+  fi
+}
+
+# The 'server_id' number for slave needs to be within 1-4294967295 range.
+# This function will take the 'hostname' if the container, hash it and turn it
+# into the number.
+# See: https://dev.mysql.com/doc/refman/en/replication-options.html#option_mysqld_server-id
+function server_id() {
+  checksum=$(sha256sum <<< $(hostname -i))
+  checksum=${checksum:0:14}
+  echo -n $((0x${checksum}%4294967295))
+}
+
+function wait_for_mysql_master() {
+  while true; do
+    echo "Waiting for MySQL master (${MYSQL_MASTER_SERVICE_NAME}) to accept connections ..."
+    mysqladmin --host=${MYSQL_MASTER_SERVICE_NAME} --user="${MYSQL_MASTER_USER}" \
+      --password="${MYSQL_MASTER_PASSWORD}" ping &>/dev/null && return 0
+    sleep 1
+  done
+}
+
+
